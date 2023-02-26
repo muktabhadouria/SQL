460a92d451f109b3e118e255d0cef66846d76c1f
Mukta Bhadouria <91377180+muktabhadouria@users.noreply.github.com>
2023-02-26 23:29:17 +0530

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,11 @@
+--select*from EmpData1;
+--select top 5 *from EmpData1;
+--select distinct(EmployeeAge) from EmpData1;
+--select distinct(EmployeeGender) from EmpData1;
+--select count(EmployeeName) as lastname
+--from EmpData1;
+--select max(EmployeeAge) from EmpData1;
+--select avg(EmployeeAge) from EmpData1;
+--select* from [SQL QUERIES].dbo.EmpData1;
+
+
